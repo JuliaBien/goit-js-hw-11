c68d401891e872be9c394a098b2fbfef54e3eeff
JuliaBien <150686648+JuliaBien@users.noreply.github.com>
2024-05-03 01:49:22 +0200

    up

--- src/index.js
+++ src/index.js
@@ -3,20 +3,53 @@ import axios from 'axios';
 const apiKey = '43685293-a1262c6d1e0da8b74ac4bda00';
 const apiUrl = 'https://pixabay.com/api/';
 const searchForm = document.querySelector('form.search-form');
+const gallery = document.querySelector('div.gallery');
 let enteredValue = undefined;
 
 searchForm.addEventListener('submit', event => {
   event.preventDefault();
   const form = event.target;
   enteredValue = form.elements.searchQuary.value;
-  fetchImages(enteredValue);
+  fetchImages(enteredValue).then(photos => {
+    //   if photos.hits === [] - co tu byłoby źle
+    if (photos.total === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      const photosData = photos.hits;
+      gallery.innerHTML = '';
+      photosData.forEach(photoData => {
+        const galleryElement = document.createElement('div');
+        galleryElement.classList.add('photo-card');
+        galleryElement.innerHTML = `<img src="${photoData.webformatURL}" alt="${photoData.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${photoData.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${photoData.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${photoData.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${photoData.downloads}</b>
+    </p>
+  </div>`;
+        gallery.append(galleryElement);
+      });
+    }
+  });
 });
+
 const fetchImages = async enteredValue => {
   try {
-    const photos = await axios.get(
+    const results = await axios.get(
       `${apiUrl}?key=${apiKey}&q=${enteredValue}&image_type=photo&orientation=horizontal&safesearch=true`
     );
-    return photos.data;
+    console.log(results.data);
+    return results.data;
   } catch (error) {
     Notify.failure(error.message);
   }
